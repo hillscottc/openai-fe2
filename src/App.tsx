6e21cb2db897296f77e12b84f99f0d236768b4ae
Scott Hill <scott289@gmail.com>
2025-01-26 20:22:19 -0800

    set up theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import ChatBot from "./ChatBot";
 import CssBaseline from "@mui/material/CssBaseline";
 import Footer from "./components/Footer";
 import Container from "@mui/material/Container";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
 
 const App: React.FunctionComponent = () => {
   return (
-    <React.Fragment>
-      <CssBaseline enableColorScheme />
-      <Container component="main">
-        <ChatBot />
-      </Container>
-      <Footer />
-    </React.Fragment>
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline enableColorScheme />
+        <Container component="main">
+          <ChatBot />
+        </Container>
+        <Footer />
+      </React.Fragment>
+    </ThemeProvider>
   );
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+// Import the Roboto font, the default for MUI
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
